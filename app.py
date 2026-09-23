1c9a49e931f0e7dcc597d6646cd96144d3c8c975
nampdh1-ux <nampdh1@gmail.com>
2026-09-23 22:21:08 +0700

    ok

--- app.py
+++ app.py
@@ -2372,7 +2372,7 @@ with tab1:
         # --- TAB CON 2: XỬ LÝ ẢNH CHỤP / TÀI LIỆU SCAN (CHỌN NHIỀU ẢNH CÙNG LÚC) ---
         with tab_import_img:
             emr_photos = st.file_uploader(
-                "Tải lên các trang ảnh chụp / scan hồ sơ bệnh án (Chọn nhiều ảnh một lần):",
+                "Tải lên ảnh chụp / scan bệnh án (Chọn nhiều ảnh một lần):",
                 type=["png", "jpg", "jpeg"],
                 accept_multiple_files=True,
                 key="emr_photos_batch_uploader"
